43d95e6c924dc42e4e9e28be793c61f170f4478e
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-08 12:04:22 +0530

    JG: wrap all utils into Utils folder

---
+++ Talk-is-cheap/src/utils/constants.js
@@ -0,0 +1,4 @@
+// Collection of all types of hard code strings.
+
+export const LOGO_URL = 'https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2';
+export const CDN_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660';
---
+++ Talk-is-cheap/src/utils/reslist.js
@@ -0,0 +1,361 @@
+export const resList = [
+    {
+        "info": {
+            "id": "101997",
+            "name": "Rajhans Restaurant & Thali (Ghora Nikkas)",
+            "cloudinaryImageId": "aipwx1svepzdvsreuszq",
+            "cuisines": [
+                "Burgers",
+                "Chinese",
+                "Rolls & Wraps",
+                "Pizzas",
+                "Thalis",
+                "North Indian"
+            ],
+            "avgRating": 4.1,
+            "sla": {
+                "deliveryTime": 25,
+            }
+        }
+    },
+    {
+        "info": {
+            "id": "613953",
+            "name": "Sharma & Vishnu Fast Food",
+            "cloudinaryImageId": "xtjilr4hbushoxflffjm",
+            "cuisines": [
+                "Indian",
+                "Snacks",
+                "Mughlai",
+                "Beverages",
+                "Fast Food"
+            ],
+            "avgRating": 4.3,
+            "sla": {
+                "deliveryTime": 26,
+            },
+        }
+    },
+    {
+        "info": {
+            "id": "368595",
+            "name": "Bhopal Udipi Restaurant",
+            "cloudinaryImageId": "rsk6jwh8iwou7sstdsdy",
+            "cuisines": [
+                "South Indian",
+                "Snacks",
+                "Thalis",
+                "Chinese",
+                "North Indian",
+                "Fast Food"
+            ],
+            "avgRating": 4.1,
+            "sla": {
+                "deliveryTime": 29,
+            },
+        }
+    },
+    {
+        "info": {
+            "id": "76688",
+            "name": "Guptaji Shakahari Bhojnalay",
+            "cloudinaryImageId": "jb9iyqwlhuyxyvb6zwnz",
+            "cuisines": [
+                "Thalis",
+                "North Indian",
+                "Indian",
+                "Fast Food",
+                "Chinese"
+            ],
+            "avgRating": 4,
+            "sla": {
+                "deliveryTime": 30,
+            },
+        }
+    },
+    {
+        "info": {
+            "id": "523122",
+            "name": "Domino's Pizza",
+            "cloudinaryImageId": "qajboygqykfvdlzo07gb",
+            "cuisines": [
+                "Pizzas",
+                "Italian",
+                "Pastas",
+                "Desserts"
+            ],
+            "avgRating": 4.1,
+            "sla": {
+                "deliveryTime": 25,
+            },
+        }
+    },
+    {
+        "info": {
+            "id": "109939",
+            "name": "Zam Zam Fast Food",
+            "cloudinaryImageId": "kuisp5xx9tcutesp33yf",
+            "cuisines": [
+                "North Indian",
+                "Mughlai",
+                "Indian",
+                "Chinese",
+                "Fast Food"
+            ],
+            "avgRating": 4.2,
+            "sla": {
+                "deliveryTime": 36,
+            },
+        }
+    },
+    {
+        "info": {
+            "id": "324628",
+            "name": "Sharma Bhojnalaya - Bus Stand Jawahar Chowk",
+            "cloudinaryImageId": "1641ddd08c8c4a31e86522328f5f8465",
+            "cuisines": [
+                "Thalis",
+                "North Indian",
+                "Indian",
+                "Fast Food",
+                "Chinese"
+            ],
+            "avgRating": 4.2,
+            "sla": {
+                "deliveryTime": 32,
+            },
+        }
+    },
+    {
+        "info": {
+            "id": "585068",
+            "name": "Shri Anjani Veg Bhojnalaya",
+            "cloudinaryImageId": "zzvff4q1lnybc2qogo1n",
+            "cuisines": [
+                "Thalis",
+                "North Indian",
+                "Indian",
+                "Fast Food",
+                "Chinese"
+            ],
+            "avgRating": 4,
+            "sla": {
+                "deliveryTime": 34,
+            },
+        }
+    },
+    {
+        "info": {
+            "id": "78535",
+            "name": "Mahadev Bhojnalay",
+            "cloudinaryImageId": "u3mgfammbsuwfpkaeeif",
+            "cuisines": [
+                "Thalis",
+                "North Indian",
+                "Indian",
+                "Fast Food",
+                "Chinese"
+            ],
+            "avgRating": 3.9,
+            "sla": {
+                "deliveryTime": 29,
+            },
+        }
+    },
+    {
+        "info": {
+            "id": "557933",
+            "name": "Bake N Shake",
+            "cloudinaryImageId": "ogej1e2xxku5bhfafdcw",
+            "cuisines": [
+                "Beverages",
+                "Snacks",
+                "Chinese",
+                "Fast Food",
+                "Bakery",
+                "Desserts",
+                "Continental",
+                "Pizzas",
+                "Pastas"
+            ],
+            "avgRating": 4.5,
+            "sla": {
+                "deliveryTime": 25,
+            },
+        }
+    },
+    {
+        "info": {
+            "id": "77016",
+            "name": "Bapu Ki Kutia",
+            "cloudinaryImageId": "rumvx9pfc56tbez1wovn",
+            "cuisines": [
+                "North Indian",
+                "Chinese",
+                "Beverages",
+                "Thalis",
+                "Snacks",
+                "Fast Food",
+                "South Indian"
+            ],
+            "avgRating": 4.4,
+            "sla": {
+                "deliveryTime": 24,
+            }
+        }
+    },
+    {
+        "info": {
+            "id": "113424",
+            "name": "Sagar Gaire",
+            "cloudinaryImageId": "fjguivcigogxmk3d8f7u",
+            "cuisines": [
+                "Snacks",
+                "North Indian",
+                "Chinese",
+                "South Indian"
+            ],
+            "avgRating": 4.2,
+            "sla": {
+                "deliveryTime": 23,
+            }
+        }
+    },
+    {
+        "info": {
+            "id": "156090",
+            "name": "The Good Bowl",
+            "cloudinaryImageId": "6e04be27387483a7c00444f8e8241108",
+            "cuisines": [
+                "Biryani",
+                "North Indian",
+                "Pastas",
+                "Punjabi",
+                "Desserts",
+                "Beverages"
+            ],
+            "avgRating": 4.4,
+            "sla": {
+                "deliveryTime": 25,
+            }
+        }
+    },
+    {
+        "info": {
+            "id": "107521",
+            "name": "Mughal Darbar",
+            "cloudinaryImageId": "eeeylbrnliju2q8oqykq",
+            "cuisines": [
+                "Mughlai",
+                "Tandoor",
+                "Snacks",
+                "Fast Food"
+            ],
+            "avgRating": 4,
+            "sla": {
+                "deliveryTime": 24,
+            }
+        }
+    },
+    {
+        "info": {
+            "id": "312786",
+            "name": "Ganesham Fast Food",
+            "cloudinaryImageId": "ybbwkjnc5kdc2kihwsxo",
+            "cuisines": [
+                "Chinese",
+                "South Indian",
+                "American",
+                "Italian",
+                "Pizzas",
+                "Pastas",
+                "Fast Food",
+                "Snacks",
+                "Beverages"
+            ],
+            "avgRating": 4.2,
+            "sla": {
+                "deliveryTime": 24,
+            }
+        }
+    },
+    {
+        "info": {
+            "id": "78534",
+            "name": "Da Pizzeria",
+            "cloudinaryImageId": "hufbd0lfid2pub8ushmb",
+            "cuisines": [
+                "Pizzas",
+                "Pastas",
+                "Italian",
+                "Snacks",
+                "Beverages",
+                "Desserts"
+            ],
+            "avgRating": 4.2,
+            "sla": {
+                "deliveryTime": 35,
+            }
+        }
+    },
+    {
+        "info": {
+            "id": "703152",
+            "name": "Indian Coffee House",
+            "cloudinaryImageId": "d088b6ece631fd100ead7b0cdaf93047",
+            "cuisines": [
+                "North Indian",
+                "Thalis",
+                "Indian",
+                "Fast Food",
+                "Chinese"
+            ],
+            "avgRating": 4.2,
+            "sla": {
+                "deliveryTime": 25,
+            }
+        }
+    },
+    {
+        "info": {
+            "id": "386982",
+            "name": "Manohar Dairy",
+            "cloudinaryImageId": "onrthurhbk4ztdnbbpwh",
+            "cuisines": [
+                "Chaat",
+                "Snacks",
+                "Sweets",
+                "Desserts",
+                "Beverages",
+                "Street Food",
+                "Bakery"
+            ],
+            "avgRating": 4.6,
+            "sla": {
+                "deliveryTime": 30,
+            }
+        }
+    },
+    {
+        "info": {
+            "id": "82465",
+            "name": "New Inn Restaurant",
+            "cloudinaryImageId": "zcbeksptp9imnvvygmrn",
+            "cuisines": [
+                "South Indian",
+                "North Indian",
+                "Chinese",
+                "Mughlai",
+                "Fast Food",
+                "Thalis"
+            ],
+            "avgRating": 4.1,
+            "sla": {
+                "deliveryTime": 30,
+            }
+        }
+    }
+
+]
+
+// export default resList //it will not generate error

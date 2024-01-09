874676b61f0f0704820758f030d04b89a3120505
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-09 12:44:01 +0530

    JG: create Skeleton for loaders

---
+++ Talk-is-cheap/src/components/Simmer.js
@@ -0,0 +1,16 @@
+const Simmer = () => {
+    return (
+        <div className='simmer-container'>
+            <div className="simmer-card"></div>
+            <div className="simmer-card"></div>
+            <div className="simmer-card"></div>
+            <div className="simmer-card"></div>
+            <div className="simmer-card"></div>
+            <div className="simmer-card"></div>
+            <div className="simmer-card"></div>
+            <div className="simmer-card"></div>
+        </div>
+    )
+}
+
+export default Simmer;

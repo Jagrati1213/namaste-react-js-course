d0b865ba96e45c154995165f5920b9bfd573ad61
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-13 18:18:57 +0530

    JG: add Price element

--- Talk-is-cheap/src/components/ResCrad.js
+++ Talk-is-cheap/src/components/ResCrad.js
@@ -2,14 +2,14 @@ import { CDN_URL } from "../utils/constants";
 
 const ResCard = ({ data }) => {
     // object destructuring
-    const { avgRating, name, cuisines, cloudinaryImageId, sla } = data?.info;
-
+    const { avgRating, name, cuisines, cloudinaryImageId, sla, costForTwo } = data?.info;
     return (
         <div className='res-card'>
             <img src={`${CDN_URL}/${cloudinaryImageId}`} alt="res-logo" className='res-logo' />
             <h2>{name}</h2>
             <p>{cuisines.join(', ')}</p>
             <h4>{avgRating} ratings | {sla?.deliveryTime} minutes</h4>
+            <p>{costForTwo}</p>
         </div>
     )
 }

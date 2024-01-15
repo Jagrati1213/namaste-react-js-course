a56552547e4cbf9d35596279fc743e44c2b4390a
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-15 13:02:12 +0530

    JG: add Link to navigate the pages

--- Talk-is-cheap/src/components/Header.js
+++ Talk-is-cheap/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { LOGO_URL } from "../utils/constants";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     const [btnName, setBtnName] = useState('Login');
@@ -11,9 +12,11 @@ const Header = () => {
             </div>
             <div className="nav_links_container">
                 <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact Us</li>
+                    <li>
+                        <Link to={'/'}>Home</Link>
+                    </li>
+                    <li> <Link to={'/about'}>About</Link></li>
+                    <li> <Link to={'/contact'}>Contact</Link></li>
                     <li>Cart</li>
                     <button className='login-btn' onClick={() => btnName === 'Login' ? setBtnName('Logout') : setBtnName('Login')}>{btnName}</button>
                 </ul>

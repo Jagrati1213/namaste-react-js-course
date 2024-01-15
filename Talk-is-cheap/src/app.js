d666d61637d48ae3b93c8e252f829f3c291bd5b8
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-15 12:39:42 +0530

    JG: updates -> Create about, contact and error component. -> Use createBrowserRouter configure for multiple routing. -> render into root.

--- Talk-is-cheap/src/app.js
+++ Talk-is-cheap/src/app.js
@@ -4,7 +4,10 @@ import ReactDOM from 'react-dom/client';
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
-
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 // **** CREATE APP LAYOUT ****//
 const AppLayout = () => {
@@ -17,6 +20,22 @@ const AppLayout = () => {
     )
 }
 
+// **** CREATE CONFIGURATION FOR ROUTER **** //
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppLayout />,
+        errorElement: <Error />
+    },
+    {
+        path: '/about',
+        element: <About />
+    },
+    {
+        path: '/contact',
+        element: <Contact />
+    },
+])
 // **** RENDERING APP INTO DOM ****//
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={router} />);

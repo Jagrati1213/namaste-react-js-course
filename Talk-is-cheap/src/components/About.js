6a2ef9c6f23472d874613409299ff72dae1fbabf
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-16 16:51:27 +0530

    JG: import user class component

--- Talk-is-cheap/src/components/About.js
+++ Talk-is-cheap/src/components/About.js
@@ -1,8 +1,12 @@
+import UserClass from "./UserClass";
+import { Row } from "antd";
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <h3>React router -- About</h3>
+        <div className="about">
+            <h1 style={{ textAlign: 'center' }}>About</h1>
+            <Row justify={'center'} className="user-card-container">
+                <UserClass />
+            </Row>
         </div>
     )
 }

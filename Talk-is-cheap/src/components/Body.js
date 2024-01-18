927d8bccede404ff01c057d7c5c90d95e09efc25
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-18 13:04:37 +0530

    JG: import all things from the useSwiggyApi

--- Talk-is-cheap/src/components/Body.js
+++ Talk-is-cheap/src/components/Body.js
@@ -6,45 +6,21 @@ import Simmer from './Simmer';
 import { SWIGGY_API } from '../utils/constants';
 import { Link } from 'react-router-dom';
 import useOnlineStatus from '../utils/useOnlineStatus';
+import useSwiggyApi from '../utils/useSwiggyApi';
 
 const Body = () => {
 
-    // state for Restaurants list 
-    const [listOfRes, setListOfRes] = useState(null); //original
-    const [FilterListOfRes, setFilterListOfRes] = useState(null); //copy
-    const [searchText, setSearchText] = useState('');
+    // state for Restaurants list from custom Hook
+    const {
+        listOfRes,
+        filterListOfRes,
+        fetchData,
+        topRatedFilterHandler,
+        searchFilterResHandler,
+        searchText,
+        setSearchText } = useSwiggyApi();
+
     const onlineStatus = useOnlineStatus();
-
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    // Fetch Restaurant from swiggy api
-    const fetchData = async () => {
-
-        try {
-            const data = await fetch(SWIGGY_API);
-            const response = await data.json();
-            const fetchLists = response.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants;
-
-            setListOfRes(fetchLists);
-            setFilterListOfRes(fetchLists);
-            console.log(fetchLists);
-        } catch (error) {
-            console.error('Error fetching data from Swiggy API:', error);
-        }
-    }
-
-    // Filter out search restaurant
-    /*
-    * item.info?.name.includes() : returns all the restaurant data that includes the search text.
-    * item.info?.name == searchText = return only that data match full name with search value.
-    */
-
-    const searchFilterResHandler = () => {
-        const searchRes = listOfRes.filter((item) => item.info?.name.toLowerCase().includes(searchText.toLowerCase()));
-        return setFilterListOfRes(searchRes);
-    }
 
     if (onlineStatus === false) return <h1>Looks you are offline, please check your connection</h1>
 
@@ -64,14 +40,7 @@ const Body = () => {
                 </div>
 
                 {/* Filter Button */}
-                <button className='filter-btn' onClick={() => {
-                    // filter logic
-                    const filterResList = listOfRes.filter((item) => {
-                        return item.info.avgRating > 4.3;
-                    });
-                    setFilterListOfRes(filterResList);
-
-                }}>
+                <button className='filter-btn' onClick={topRatedFilterHandler}>
                     Top Rated Restaurants
                 </button>
             </div>
@@ -79,8 +48,8 @@ const Body = () => {
             {/* Restaurant container */}
             <div className="res-container">
                 {
-                    FilterListOfRes?.length == 0 ? <h1 style={{ color: 'black' }}>Not found</h1> :
-                        FilterListOfRes?.map((item) => {
+                    filterListOfRes === null ? <h1 style={{ color: 'black' }}>Not found</h1> :
+                        filterListOfRes?.map((item) => {
                             return (
                                 <Link key={item?.info.id} to={`/restaurants/${item?.info.id}`}>
                                     <ResCard data={item} />

3ae17328065fb8f2b8b7a5bc35dc601769b3d1a3
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-21 18:32:26 +0530

    JG: import context value in header

--- Talk-is-cheap/src/components/Header.js
+++ Talk-is-cheap/src/components/Header.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { LOGO_URL } from "../utils/constants";
 import { Link } from 'react-router-dom';
+import CurrentUserContext from '../utils/Context';
+
 
 const Header = () => {
     const [btnName, setBtnName] = useState('Login');
+    const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
     console.log('Run whenever state changed');
     return (
         <header className='header'>
@@ -12,12 +15,10 @@ const Header = () => {
             </div>
             <div className="nav_links_container">
                 <ul>
-                    <li>
-                        <Link to={'/'}>Home</Link>
-                    </li>
+                    <li><Link to={'/'}>Home</Link></li>
                     <li> <Link to={'/about'}>About</Link></li>
                     <li> <Link to={'/contact'}>Contact</Link></li>
-                    <li>Cart</li>
+                    <li>{currentUser === null ? '' : currentUser}</li>
                     <button className='login-btn' onClick={() => btnName === 'Login' ? setBtnName('Logout') : setBtnName('Login')}>{btnName}</button>
                 </ul>
             </div>

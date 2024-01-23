8852469bb619923282a46348e7172d67e6a9f69e
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-23 20:13:02 +0530

    JG: change the order

--- Talk-is-cheap/src/utils/useRestaurantMenu.js
+++ Talk-is-cheap/src/utils/useRestaurantMenu.js
@@ -6,20 +6,17 @@ const useRestaurantMenu = ({ resId }) => {
     // state for handling menuList
     const [resMenuList, setResMenuList] = useState(null);
 
+    // fetch Menu list
+    const fetchMenuList = async () => {
+        const response = await fetch(`${SWIGGY_MENU_API}${resId}`);
+        const json = await response?.json();
+        setResMenuList(json?.data);
+    }
     // Render only once 
     useEffect(() => {
         window.scrollTo(0, 0);
         fetchMenuList();
     }, []);
-
-    // fetch Menu list
-    const fetchMenuList = async () => {
-        const response = await fetch(`${SWIGGY_MENU_API}${resId}`);
-        const json = await response.json();
-        setResMenuList(json?.data);
-        console.log(json.data);
-    }
-
     return resMenuList;
 }
 

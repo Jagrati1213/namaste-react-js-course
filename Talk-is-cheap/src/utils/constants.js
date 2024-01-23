9af3b8dbb0827b59eef7e1484f375ca6e93e2184
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-23 20:12:38 +0530

    JG: change url for API

--- Talk-is-cheap/src/utils/constants.js
+++ Talk-is-cheap/src/utils/constants.js
@@ -2,5 +2,6 @@
 
 export const LOGO_URL = 'https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?size=2';
 export const CDN_URL = 'https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660';
-export const SWIGGY_API = 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.2599333&lng=77.412615&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING';
-export const SWIGGY_MENU_API = 'https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=23.2599333&lng=77.412615&restaurantId=';
+export const SWIGGY_API = 'https://foodfire.onrender.com/api/restaurants?lat=21.1702401&lng=72.83106070000001&page_type=DESKTOP_WEB_LISTING';
+
+export const SWIGGY_MENU_API = 'https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId=';

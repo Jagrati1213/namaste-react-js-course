b134c595ef7ea6ccb4dece018bf44d89ee2b61e2
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-23 20:09:26 +0530

    JG: change into h2 tag

--- Talk-is-cheap/src/components/Contact.js
+++ Talk-is-cheap/src/components/Contact.js
@@ -2,7 +2,7 @@ const Contact = () => {
     return (
         <div>
             <h1>Contact</h1>
-            <h3>React router -- contact</h3>
+            <h2>React router -- contact</h2>
         </div>
     )
 }

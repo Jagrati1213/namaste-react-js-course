236090daee827c07d5c05a262fcbbcde5e6b4525
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-24 17:32:43 +0530

    JG: setup our app layout

--- snap-gpt/src/App.js
+++ snap-gpt/src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import Body from './components/body/Body';
+import Header from './components/header/Header';
+import { SNAP_GPT_BACKGROUND_IMG } from './utils/Constant';
 
 function App() {
+
   return (
-    <div className="App font-serif text-xl text-gray-900 font-bold">
-      hello SnapGTP
+    <div className='app w-full bg-gray-950 min-h-[100vh]'>
+      <Header />
+      <div className='h-fit'>
+        <img src={SNAP_GPT_BACKGROUND_IMG} alt="background" className='absolute top-0 opacity-50 h-full w-full object-cover' />
+      </div>
+      <Body />
+      <div>Footer</div>
     </div>
   );
 }

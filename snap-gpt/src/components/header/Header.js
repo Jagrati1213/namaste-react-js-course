1c0549cd425594567b5c81fb68ea560176344196
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-24 17:32:14 +0530

    JG: create Header

---
+++ snap-gpt/src/components/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header className='p-5 relative z-10'>
+            <h3 className='text-3xl md:text-4xl lg:text-5xl font-extrabold italic text-green-500 tracking-wide'>SnapGPT</h3>
+        </header>
+    )
+}
+
+export default Header

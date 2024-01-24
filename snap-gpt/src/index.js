4507ffd6040d7995f5cc5ac9d062260f8826866c
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-24 17:33:01 +0530

    JG: create routing and wrap into app

--- snap-gpt/src/index.js
+++ snap-gpt/src/index.js
@@ -3,15 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Login from './components/login/Login';
+import Browse from './components/browse/Browse';
+
+// create routing for web
+const appRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Login />
+      },
+      {
+        path: '/browse',
+        element: <Browse />
+      }
+    ]
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={appRouter}> <App /></RouterProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

b22a805afaaee2d55a2dbeb44b1e37cccd14b9d0
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-24 18:20:20 +0530

    JG: change name of header logo

--- snap-gpt/src/components/header/Header.js
+++ snap-gpt/src/components/header/Header.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Header() {
     return (
-        <header className='p-5 relative z-10'>
-            <h3 className='text-3xl md:text-4xl lg:text-5xl font-extrabold italic text-green-500 tracking-wide'>SnapGPT</h3>
+        <header className='p-5 relative z-10 bg-gradient-to-b from-black'>
+            <h3 className='text-3xl md:text-4xl lg:text-5xl font-extrabold italic text-green-500 tracking-wide'>FilmChicks</h3>
         </header>
     )
 }

c9184913627921bb9b10910d146a7440bb4eae91
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-25 17:13:15 +0530

    JG: show the display name

--- snap-gpt/src/components/browse/Browse.js
+++ snap-gpt/src/components/browse/Browse.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function Browse() {
+    const user = useSelector((store) => store.user);
     return (
-        <div>Browse</div>
+        <div className='text-white'>{user.displayName}</div>
     )
 }
 

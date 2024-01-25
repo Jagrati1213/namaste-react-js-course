1ba153e6915dd7e24c439ad551231babbdd6bafe
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-25 11:43:06 +0530

    JG: change name of background image

--- snap-gpt/src/utils/Constant.js
+++ snap-gpt/src/utils/Constant.js
@@ -1,1 +1,1 @@
-export const SNAP_GPT_BACKGROUND_IMG = 'https://assets.nflxext.com/ffe/siteui/vlv3/16006346-87f9-4226-bc25-a1fb346a2b0c/9662d0fd-0547-4665-b887-771617268815/IN-en-20240115-popsignuptwoweeks-perspective_alpha_website_large.jpg';
+export const FILM_CHICKS_BACKGROUND_IMG = 'https://assets.nflxext.com/ffe/siteui/vlv3/16006346-87f9-4226-bc25-a1fb346a2b0c/9662d0fd-0547-4665-b887-771617268815/IN-en-20240115-popsignuptwoweeks-perspective_alpha_website_large.jpg';

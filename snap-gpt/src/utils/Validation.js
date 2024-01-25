64871207bec17552c471c70cb2b9c304817bb3c9
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-25 11:40:17 +0530

    JG: create form validation

---
+++ snap-gpt/src/utils/Validation.js
@@ -0,0 +1,23 @@
+export const CheckValidationOfSignUp = (name, email, password) => {
+
+    const isFullNameValid = /(^[A-Za-z]{3,16})([ ]{0,1})([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})?([ ]{0,1})?([A-Za-z]{3,16})/.test(name);
+    const isEmailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/.test(password);
+
+    if (!isFullNameValid) return 'Name is invalid'
+    if (!isEmailValid) return 'Email is  invalid';
+    if (!isPasswordValid) return 'Password have uppercase, special char, num,& 6+ characters';
+
+    return null;
+}
+
+export const CheckValidationOfSignIn = (email, password) => {
+
+    const isEmailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email);
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/.test(password);
+
+    if (!isEmailValid) return 'Email is  invalid';
+    if (!isPasswordValid) return 'Password have uppercase, special char, num,& 6+ characters';
+
+    return null;
+}

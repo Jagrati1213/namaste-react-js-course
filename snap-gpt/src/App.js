5b2c39bf9e5bbf78d3a53f33c491d0066c458d45
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-25 11:43:19 +0530

    JG: import the bg-image

--- snap-gpt/src/App.js
+++ snap-gpt/src/App.js
@@ -1,6 +1,6 @@
 import Body from './components/body/Body';
 import Header from './components/header/Header';
-import { SNAP_GPT_BACKGROUND_IMG } from './utils/Constant';
+import { FILM_CHICKS_BACKGROUND_IMG } from './utils/Constant';
 
 function App() {
 
@@ -8,7 +8,7 @@ function App() {
     <div className='app w-full bg-black min-h-[100vh]'>
       <Header />
       <div className='h-fit'>
-        <img src={SNAP_GPT_BACKGROUND_IMG} alt="background" className='absolute top-0 opacity-[0.4] h-full w-full object-cover' />
+        <img src={FILM_CHICKS_BACKGROUND_IMG} alt="background" className='absolute top-0 opacity-[0.4] h-full w-full object-cover' />
       </div>
       <Body />
       <div>Footer</div>

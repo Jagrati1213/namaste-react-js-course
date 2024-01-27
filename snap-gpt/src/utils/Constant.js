44e135b7c4ee512fd7f71b7a37705b095767ba7c
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-27 12:27:48 +0530

    JG: Add api key and options

--- snap-gpt/src/utils/Constant.js
+++ snap-gpt/src/utils/Constant.js
@@ -1,3 +1,13 @@
 export const FILM_CHICKS_BACKGROUND_IMG = 'https://assets.nflxext.com/ffe/siteui/vlv3/16006346-87f9-4226-bc25-a1fb346a2b0c/9662d0fd-0547-4665-b887-771617268815/IN-en-20240115-popsignuptwoweeks-perspective_alpha_website_large.jpg';
 
 export const FILM_CHICKS_AVTAR_IMG = 'https://i.pinimg.com/originals/34/62/d2/3462d27440aa255b1c314ff16f4032b4.png';
+
+export const TMDB_API_KEY = '56c2da6fcd6b877bb5d204c9278cbd3b';
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NmMyZGE2ZmNkNmI4NzdiYjVkMjA0YzkyNzhjYmQzYiIsInN1YiI6IjY1YjQ5ZjJmNjBjNTFkMDE0OGQwMmNlYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.zwjTXoU3_FSOcgJycbEUkh3dLR4BrR3H6zejo5wnTO8'
+    }
+};

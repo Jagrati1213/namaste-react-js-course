cfae4341bc3680f7e024b97540d96e3e56962da5
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-27 12:34:05 +0530

    JG: create custom hook -> Fetch the data from TMDB -> Set the data into movieSlice

---
+++ snap-gpt/src/utils/hooks/useGetNowPlayingMovies.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { API_OPTIONS } from "../Constant";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../redux/slice/MovieSlice";
+
+/** 
+ * A function that fetch NowPlayingMovies from TMDB API, 
+ * and set the fetch data into movie's Redux store.
+ * */
+export const useGetNowPlayingMovies = () => {
+
+    const dispatch = useDispatch();
+
+    // Function to fetch TMDB data && set into store
+    const handleGetNowPlayingMovie = async () => {
+        const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?&page=1', API_OPTIONS);
+        const data = await response.json();
+        dispatch(addMovies(data?.results));
+    }
+
+    // Called method after component mount
+    useEffect(() => {
+        handleGetNowPlayingMovie();
+    }, []);
+}

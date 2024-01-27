b8841c61c8454d1cfed55c84331cd8644aeb910d
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-27 12:43:13 +0530

    JG: call the custom hook

--- snap-gpt/src/components/browse/Browse.js
+++ snap-gpt/src/components/browse/Browse.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { useGetNowPlayingMovies } from '../../utils/hooks/useGetNowPlayingMovies';
 
 function Browse() {
+
     const user = useSelector((store) => store.user);
+    // Called the method
+    useGetNowPlayingMovies();
+
     return (
-        <div className='text-white'>{user.displayName}</div>
+        <div className='text-white'>{user?.displayName}</div>
     )
 }
 

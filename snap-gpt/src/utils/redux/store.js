8b15baea6e790eacc9f793cf734961bfc90deac6
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-27 12:27:28 +0530

    JG: import the movie slice into store

--- snap-gpt/src/utils/redux/store.js
+++ snap-gpt/src/utils/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import UserSlice from './slice/UserSlice';
+import MovieSlice from './slice/MovieSlice';
 
 export const appStore = configureStore({
     reducer: {
         user: UserSlice,
+        movies: MovieSlice
     }
 })

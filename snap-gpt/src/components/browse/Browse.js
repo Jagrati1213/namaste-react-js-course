8bb712395518ba37f0a10e7865fe2daaea315ee6
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 16:48:57 +0530

    JG: call the reducer

--- snap-gpt/src/components/browse/Browse.js
+++ snap-gpt/src/components/browse/Browse.js
@@ -3,12 +3,16 @@ import { useSelector } from 'react-redux'
 import { useGetNowPlayingMovies } from '../../utils/hooks/useGetNowPlayingMovies';
 import { MainContainer } from '../mainContainer/MainContainer';
 import { SecondaryContainer } from '../secondaryContainer/SecondaryContainer';
+import { useGetTopRatedMovie } from '../../utils/hooks/useGetTopRatedMovie';
+import { useGetUpComingMovies } from '../../utils/hooks/useGetUpComingMovies';
 
 function Browse() {
 
     const user = useSelector((store) => store.user);
     // Called the method
     useGetNowPlayingMovies();
+    useGetTopRatedMovie();
+    useGetUpComingMovies();
 
     return (
         <div>

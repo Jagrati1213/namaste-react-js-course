48d081fbcfd63a1654f456632ba8768104d4f799
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 16:19:08 +0530

    Jg: add width

--- snap-gpt/src/components/mainContainer/MainContainer.js
+++ snap-gpt/src/components/mainContainer/MainContainer.js
@@ -12,7 +12,7 @@ export const MainContainer = () => {
     const { original_title, overview, id } = mainMovie;
 
     return (
-        <div className="text-white h-full overflow-y-hidden">
+        <div className="text-white w-full h-full overflow-hidden">
             <VideoTitle title={original_title} overview={overview} />
             <VideoBackground movieID={id} />
         </div>

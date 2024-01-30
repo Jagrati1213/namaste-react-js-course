46f9f3debb0dce019aff9ab683ba060e09a0e1c8
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 17:28:25 +0530

    JG: create secondary container

---
+++ snap-gpt/src/components/secondaryContainer/SecondaryContainer.js
@@ -0,0 +1,22 @@
+import { MovieList } from "./MovieList"
+import { useSelector } from "react-redux";
+
+export const SecondaryContainer = () => {
+
+    // Get the state
+    const { nowPlayingMovies, topRatedMovies, upComingMovies } = useSelector((store) => store.movies);
+
+    // If they not present return;
+    if (!nowPlayingMovies && !topRatedMovies && !upComingMovies) return;
+
+    return (
+        <div className="bg-black text-white">
+            <div className="relative z-20 -mt-52">
+                < MovieList title={'Now Playing'} movies={nowPlayingMovies} />
+                < MovieList title={'Top Rated'} movies={topRatedMovies} />
+                < MovieList title={'Upcoming'} movies={upComingMovies} />
+            </div>
+
+        </div >
+    )
+}

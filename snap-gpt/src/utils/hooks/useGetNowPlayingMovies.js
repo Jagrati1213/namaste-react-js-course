02f2d2ce92f98be03f52ec8e77b6d9e50c2a6d5b
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 16:46:52 +0530

    Jg: change the name of reducer

--- snap-gpt/src/utils/hooks/useGetNowPlayingMovies.js
+++ snap-gpt/src/utils/hooks/useGetNowPlayingMovies.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../Constant";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../redux/slice/MovieSlice";
+import { addNowPlayingMovies } from "../redux/slice/MovieSlice";
 
 /** 
  * A function that fetch NowPlayingMovies from TMDB API, 
@@ -15,7 +15,7 @@ export const useGetNowPlayingMovies = () => {
     const handleGetNowPlayingMovie = async () => {
         const response = await fetch('https://api.themoviedb.org/3/movie/now_playing?&page=1', API_OPTIONS);
         const data = await response.json();
-        dispatch(addMovies(data?.results));
+        dispatch(addNowPlayingMovies(data?.results));
     }
 
     // Called method after component mount

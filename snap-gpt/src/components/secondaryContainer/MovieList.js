70fa543b907f2adbd8de7ac8cde396ddd5db85cd
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 17:28:10 +0530

    JG: create movie list

---
+++ snap-gpt/src/components/secondaryContainer/MovieList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { MovieCard } from './MovieCard';
+
+function MovieList({ title, movies }) {
+
+    if (!movies) return;
+
+    return (
+        <div className='px-10'>
+            <div className='py-10 text-2xl font-bold'>
+                <h1>{title}</h1>
+            </div>
+            <div className='flex overflow-x-scroll gap-4'>
+                {
+                    movies?.map((movie) => {
+                        return <MovieCard key={movie?.id} imageUrl={movie?.poster_path} />
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export { MovieList };

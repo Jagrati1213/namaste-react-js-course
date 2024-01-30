3cb0d79e3b11ba0ee568b85351af0eee1448a0b9
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 15:03:16 +0530

    JG: change the image link

--- snap-gpt/src/utils/Constant.js
+++ snap-gpt/src/utils/Constant.js
@@ -1,4 +1,4 @@
-export const FILM_CHICKS_BACKGROUND_IMG = 'https://assets.nflxext.com/ffe/siteui/vlv3/16006346-87f9-4226-bc25-a1fb346a2b0c/9662d0fd-0547-4665-b887-771617268815/IN-en-20240115-popsignuptwoweeks-perspective_alpha_website_large.jpg';
+export const FILM_CHICKS_BACKGROUND_IMG = 'https://png.pngtree.com/background/20230616/original/pngtree-wall-of-movie-posters-on-display-picture-image_3620105.jpg';
 
 export const FILM_CHICKS_AVTAR_IMG = 'https://i.pinimg.com/originals/34/62/d2/3462d27440aa255b1c314ff16f4032b4.png';
 

56c41e7f1c7c956e5334dcfb88618d8dfb43322b
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 22:45:36 +0530

    JG: create changes based on selected language

---
+++ snap-gpt/src/utils/langConstant.js
@@ -0,0 +1,10 @@
+export const langConstant = {
+    en: {
+        search: 'search',
+        gptPlaceHolder: 'What would you like to watch today?'
+    },
+    hindi: {
+        search: 'खोज',
+        gptPlaceHolder: 'आज आप क्या देखना चाहेंगे?'
+    }
+}

8e329d3c6046cba3a254bdfd2094822c32796e97
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 17:27:36 +0530

    JG: add bg-color

--- snap-gpt/src/components/mainContainer/MainContainer.js
+++ snap-gpt/src/components/mainContainer/MainContainer.js
@@ -12,7 +12,7 @@ export const MainContainer = () => {
     const { original_title, overview, id } = mainMovie;
 
     return (
-        <div className="text-white w-full h-full overflow-hidden">
+        <div className="text-white w-full h-full overflow-hidden bg-black">
             <VideoTitle title={original_title} overview={overview} />
             <VideoBackground movieID={id} />
         </div>

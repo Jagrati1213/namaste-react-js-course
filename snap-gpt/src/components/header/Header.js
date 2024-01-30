b3562fbad148df1afb6b7fa5eba0301aff59096e
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 18:26:52 +0530

    JG: add functionality in get recommendation button

--- snap-gpt/src/components/header/Header.js
+++ snap-gpt/src/components/header/Header.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
-import { FILM_CHICKS_AVTAR_IMG } from '../../utils/Constant'
 import { useDispatch, useSelector } from 'react-redux';
 import { removeUser } from '../../utils/redux/slice/UserSlice';
+import { toggleRecommendationView } from '../../utils/redux/slice/GptSlice';
 import { signOut } from "firebase/auth";
 import { auth } from '../../utils/firebase/Firebase';
 function Header() {
 
     const dispatch = useDispatch();
-    const user = useSelector((store) => store.user);
+    const { user, gpt } = useSelector((store) => store);
+
     const navigate = useNavigate();
 
     // Function for sign out
@@ -21,16 +22,25 @@ function Header() {
             navigate('/error');
         });
     }
+
+    // Function for toggle the view 
+    const handleToggleRecommendationView = () => {
+        dispatch(toggleRecommendationView());
+    }
+
     return (
         <header className='fixed top-0 p-5 w-full z-20 bg-gradient-to-b from-black flex justify-between items-center text-white'>
             <h3 className='text-3xl md:text-4xl lg:text-5xl font-extrabold italic text-green-500 tracking-wide'>FilmChicks</h3>
             {
                 user !== null &&
-                (<div className="container_of_avatar_and_signout flex">
+                (<div className="container_of_avatar_and_signout flex gap-3">
+                    <button onClick={handleToggleRecommendationView} className='px-6 py-2 bg-yellow-700 hover:bg-yellow-600 font-bold text-white rounded-sm mr-2'>
+                        {!gpt.showRecommendation ? 'Get Recommendation' : 'Back to Home'}
+                    </button>
                     <div className='bg-red-600 w-10 rounded'>
                         <img src={user.photoURL} className='w-full object-cover rounded' alt="film_clicks_avatar" />
                     </div>
-                    <button onClick={handleSignOut} className='bg-green-600 rounded px-4 mx-5 font-bold'>SignOut</button>
+                    <button onClick={handleSignOut} className='bg-green-600 hover:bg-green-700 rounded px-4 font-bold'>SignOut</button>
                 </div>)
             }
 

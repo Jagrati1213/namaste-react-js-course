bf8f237e765f8b11f98d1642d951c2e48acefe56
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 22:43:01 +0530

    JG: add layout

--- snap-gpt/src/components/recommendation/Recommendation.js
+++ snap-gpt/src/components/recommendation/Recommendation.js
@@ -1,7 +1,12 @@
+import { FILM_CHICKS_BACKGROUND_IMG } from "../../utils/Constant"
+import { MovieSuggest } from "./MovieSuggest"
+import { SearchBar } from "./SearchBar"
+
 export const Recommendation = () => {
     return (
-        <div>
-            Recommendation
+        <div className="bg-black pt-32 min-h-screen text-white flex flex-col gap-10">
+            <SearchBar />
+            <MovieSuggest />
         </div>
     )
 }

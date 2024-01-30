3319167d8247e0f145f269a0d72dfbb5ddb26c8f
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 15:15:15 +0530

    JG: create Main container

---
+++ snap-gpt/src/components/browse/MainContainer.js
@@ -0,0 +1,20 @@
+import { useSelector } from "react-redux"
+import { VideoBackground } from "./VideoBackground"
+import { VideoTitle } from "./VideoTitle"
+
+export const MainContainer = () => {
+    const nowPlayingMovies = useSelector((store) => store.movies.nowPlayingMovies);
+
+    // early return
+    if (!nowPlayingMovies) return;
+
+    const mainMovie = nowPlayingMovies[0];
+    const { original_title, overview, id } = mainMovie;
+
+    return (
+        <div className="text-white absolute top-0 h-screen">
+            <VideoTitle title={original_title} overview={overview} />
+            <VideoBackground movieID={id} />
+        </div>
+    )
+}

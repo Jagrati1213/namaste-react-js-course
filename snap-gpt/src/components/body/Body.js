661a3bff8a9de93d45fd5ea3d94b3a71a80a1a86
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 15:06:39 +0530

    JG: remove the div tag

--- snap-gpt/src/components/body/Body.js
+++ snap-gpt/src/components/body/Body.js
@@ -4,9 +4,7 @@ import { Outlet } from 'react-router-dom'
 // Outlet :- manage what will user show in different pages.
 function Body() {
     return (
-        <div>
-            <Outlet />
-        </div>
+        <Outlet />
     )
 }
 

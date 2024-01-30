7f13df03ddd5a6ca48e1bb6e4e7085205f8041a7
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-30 15:05:54 +0530

    JG: Updates ->create trailerVideo variable in store -> Add reducer for add trailer video

--- snap-gpt/src/utils/redux/slice/MovieSlice.js
+++ snap-gpt/src/utils/redux/slice/MovieSlice.js
@@ -3,14 +3,18 @@ import { createSlice } from '@reduxjs/toolkit';
 const MovieSlice = createSlice({
     name: 'movies',
     initialState: {
-        nowPlayingMovies: null
+        nowPlayingMovies: null,
+        trailerVideo: null,
     },
     reducers: {
         addMovies: (state, action) => {
             state.nowPlayingMovies = action.payload;
+        },
+        addTrailerVideo: (state, action) => {
+            state.trailerVideo = action.payload;
         }
     }
 });
 
-export const { addMovies } = MovieSlice.actions;
+export const { addMovies, addTrailerVideo } = MovieSlice.actions;
 export default MovieSlice.reducer;

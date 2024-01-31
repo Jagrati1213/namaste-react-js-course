e1f742a49d6d6b73709352298893e47fff646d4a
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-31 15:37:39 +0530

    JG: change the name

--- snap-gpt/src/components/recommendation/Recommendation.js
+++ snap-gpt/src/components/recommendation/Recommendation.js
@@ -1,12 +1,11 @@
-import { FILM_CHICKS_BACKGROUND_IMG } from "../../utils/Constant"
-import { MovieSuggest } from "./MovieSuggest"
+import { MovieSuggestList } from "./MovieSuggestList"
 import { SearchBar } from "./SearchBar"
 
 export const Recommendation = () => {
     return (
         <div className="bg-black pt-32 min-h-screen text-white flex flex-col gap-10">
             <SearchBar />
-            <MovieSuggest />
+            <MovieSuggestList />
         </div>
     )
 }

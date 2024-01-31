874aa55d562fb0f3a873f7574c9e2886ff92d4fc
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-31 16:25:00 +0530

    JG: create movie suggest container

---
+++ snap-gpt/src/components/recommendation/MovieSuggestList.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { MovieList } from '../secondaryContainer/MovieList';
+
+export const MovieSuggestList = () => {
+
+    // Get state from store
+    const { gptMoviesNames, gptMoviesResult } = useSelector((store) => store.gpt);
+
+    if (!gptMoviesNames || !gptMoviesResult) return null;
+
+    return (
+        <div className='px-6'>
+            {
+                gptMoviesNames?.map((item, index) => < MovieList key={index} title={item} movies={gptMoviesResult[index]} />)
+            }
+
+        </div>
+    )
+}
+

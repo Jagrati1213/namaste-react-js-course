23c85a9c54a519e9fb214d5d1b785476e1dca382
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-31 15:35:20 +0530

    JG: optimize the search && add recommendation functionality

--- snap-gpt/src/components/recommendation/SearchBar.js
+++ snap-gpt/src/components/recommendation/SearchBar.js
@@ -1,19 +1,32 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
+import React, { useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
 import { langConstant } from '../../utils/langConstant'
+import { addGptSearch } from '../../utils/redux/slice/GptSlice';
+import { getTmdbRecommendation } from '../../utils/helper/getTmdbRecommendation';
+
 
 export const SearchBar = () => {
 
     const config = useSelector((store) => store.config);
+    const searchRef = useRef();
+    const dispatch = useDispatch();
+
 
     // Function run after form submit
-    const handleOnsubmit = (e) => {
+    const handleOnsubmit = async (e) => {
+        // Make onload none
         e.preventDefault();
+
+        // Get TMDB data from the method
+        const data = await getTmdbRecommendation(searchRef.current.value);
+
+        // Set Into Store
+        dispatch(addGptSearch({ moviesName: data[1], moviesResult: data[0] }));
     }
     return (
         <div className='w-full flex justify-center'>
             <form onSubmit={(event) => handleOnsubmit(event)} className='justify-between gap-5 bg-yellow-900 w-1/2 rounded-sm grid grid-cols-12'>
-                <input type="text" placeholder={langConstant?.[config.lang].gptPlaceHolder} className='col-span-9 text-lg py-3 px-2 outline-none m-2 rounded-sm bg-transparent placeholder:text-white' />
+                <input type="text" ref={searchRef} placeholder={langConstant?.[config.lang].gptPlaceHolder} className='col-span-9 text-lg py-3 px-2 outline-none m-2 rounded-sm bg-transparent placeholder:text-white' />
                 <button className='col-span-3 py-4 px-10 font-bold text-lg bg-yellow-800 hover:bg-yellow-700 rounded-sm tracking-wide'>{langConstant?.[config.lang].search}</button>
             </form>
         </div>

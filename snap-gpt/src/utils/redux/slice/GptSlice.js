a204af3325386965782c3585c46e0b554c6c45e9
Jagrati Gupta <123jagratigupta@gmail.com>
2024-01-31 14:53:50 +0530

    JG: add state for gpt movies

--- snap-gpt/src/utils/redux/slice/GptSlice.js
+++ snap-gpt/src/utils/redux/slice/GptSlice.js
@@ -4,13 +4,20 @@ const GptSlice = createSlice({
     name: 'GptSlice',
     initialState: {
         showRecommendation: false,
+        gptMoviesNames: null,
+        gptMoviesResult: null,
     },
     reducers: {
         toggleRecommendationView: (state, _) => {
             state.showRecommendation = !state.showRecommendation;
+        },
+        addGptSearch: (state, action) => {
+            const { moviesName, moviesResult } = action.payload;
+            state.gptMoviesNames = moviesName;
+            state.gptMoviesResult = moviesResult;
         }
     }
 });
 
-export const { toggleRecommendationView } = GptSlice.actions;
+export const { toggleRecommendationView, addGptSearch } = GptSlice.actions;
 export default GptSlice.reducer;

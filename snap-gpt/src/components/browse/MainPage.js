bd3b998a630e83108c6ec34d444f28e77e251446
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-01 18:33:38 +0530

    JG: send props into mainContainer

--- snap-gpt/src/components/browse/MainPage.js
+++ snap-gpt/src/components/browse/MainPage.js
@@ -8,13 +8,21 @@ export const MainPage = () => {
 
     // Get Gpt from the store state
     const gpt = useSelector((store) => store.gpt);
+    const nowPlayingMovies = useSelector((store) => store.movies.nowPlayingMovies);
+
+    // early return
+    if (!nowPlayingMovies) return;
+
+    const mainMovie = nowPlayingMovies[0];
+    const { original_title, overview, id } = mainMovie;
+
     return (
         <>
             {
                 gpt.showRecommendation === true ?
                     <Recommendation /> :
                     <>
-                        <MainContainer />
+                        <MainContainer original_title={original_title} overview={overview} id={id} />
                         <SecondaryContainer />
                     </>
             }

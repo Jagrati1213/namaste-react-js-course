f917987a4f6fc7e824ff681b5aee8ca4c40065ce
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-01 17:09:42 +0530

    JG: change def

--- snap-gpt/src/utils/helper/getMovieDetail.js
+++ snap-gpt/src/utils/helper/getMovieDetail.js
@@ -1,8 +1,7 @@
 import { API_OPTIONS } from "../Constant"
 
 /**
-*  A function that fetch given movie name details from TMDB movie search API, 
-* and set the fetched data into gpt's Redux store.
+*  A function that fetch given movie name details from TMDB movie search API.
 *
 * @params movieName : A function that receives the movieName, that responsible for giving result movies details.
 * @return result : A function return the collection of related search movie.

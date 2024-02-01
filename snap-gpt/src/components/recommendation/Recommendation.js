c36dd3005be7f9c9e62b8fb4f4511de89fa7e11a
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-01 13:41:11 +0530

    JG: add bottom padding

--- snap-gpt/src/components/recommendation/Recommendation.js
+++ snap-gpt/src/components/recommendation/Recommendation.js
@@ -3,7 +3,7 @@ import { SearchBar } from "./SearchBar"
 
 export const Recommendation = () => {
     return (
-        <div className="bg-black pt-32 min-h-screen text-white flex flex-col gap-10">
+        <div className="bg-black py-32 min-h-screen text-white flex flex-col gap-10">
             <SearchBar />
             <MovieSuggestList />
         </div>

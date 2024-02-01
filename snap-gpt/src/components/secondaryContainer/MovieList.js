173fc0480a0791194aed4fdacb00b934ec792f8a
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-01 17:52:30 +0530

    JG: add navigation && set movie details and casts

--- snap-gpt/src/components/secondaryContainer/MovieList.js
+++ snap-gpt/src/components/secondaryContainer/MovieList.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import { MovieCard } from './MovieCard';
+import { Link } from 'react-router-dom';
+import { getMovieDetail } from '../../utils/helper/getMovieDetail';
+import { useDispatch } from 'react-redux';
+import { addSingleMovieDetails } from '../../utils/redux/slice/MovieSlice';
+import { getMovieCast } from '../../utils/helper/getMovieCast';
 
 function MovieList({ title, movies }) {
 
+    const dispatch = useDispatch();
     if (!movies) return;
+
+    // Get Single Movies details
+    const getData = async (arr) => {
+        const movies = await getMovieDetail(arr[0]);
+        const casts = await getMovieCast(arr[1]);
+
+        // filter the related movie details and set into movies store 
+        const singleMovieDetails = movies.find((item) => item?.original_title === arr[0]);
+        dispatch(addSingleMovieDetails({ movie: singleMovieDetails, casts: casts }));
+    }
 
     return (
         <div className='px-4 md:px-10'>
@@ -13,7 +29,10 @@ function MovieList({ title, movies }) {
             <div className={`flex ${movies.length > 4 ? 'overflow-x-scroll' : ''} overflow-y-hidden gap-4`}>
                 {
                     movies?.map((movie) => {
-                        return <MovieCard key={movie?.id} imageUrl={movie?.poster_path} />
+                        return <Link key={movie?.id} to={`/browse/${movie?.id}`}
+                            onClick={getData.bind(null, [movie?.original_title, movie?.id])}>
+                            <MovieCard imageUrl={movie?.poster_path} />
+                        </Link>
                     })
                 }
             </div>

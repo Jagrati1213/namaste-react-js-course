eff9319ac20ffe320c19e0d44e91de95c8e453dd
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-01 18:33:02 +0530

    JG: make dynamic

--- snap-gpt/src/components/mainContainer/MainContainer.js
+++ snap-gpt/src/components/mainContainer/MainContainer.js
@@ -1,15 +1,7 @@
-import { useSelector } from "react-redux"
 import { VideoBackground } from "./VideoBackground"
 import { VideoTitle } from "./VideoTitle"
 
-export const MainContainer = () => {
-    const nowPlayingMovies = useSelector((store) => store.movies.nowPlayingMovies);
-
-    // early return
-    if (!nowPlayingMovies) return;
-
-    const mainMovie = nowPlayingMovies[0];
-    const { original_title, overview, id } = mainMovie;
+export const MainContainer = ({ original_title, overview, id }) => {
 
     return (
         <div className="text-white w-full h-full overflow-hidden bg-black relative">

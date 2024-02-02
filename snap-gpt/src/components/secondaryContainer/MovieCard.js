f0d14c6e4c140ea462414866d2caee098d7d207f
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-02 12:38:58 +0530

    JG: return element if there is no poster image

--- snap-gpt/src/components/secondaryContainer/MovieCard.js
+++ snap-gpt/src/components/secondaryContainer/MovieCard.js
@@ -1,15 +1,12 @@
 import React from 'react'
-import { FILM_CHICKS_BACKGROUND_IMG, TMDB_IMAGE_URL } from '../../utils/Constant'
+import { TMDB_IMAGE_URL } from '../../utils/Constant'
 
 function MovieCard({ imageUrl }) {
-
+    if (!imageUrl) return
     return (
         <div className='flex'>
             <div className='w-32 md:w-48 h-[200px] md:h-[275px] overflow-hidden'>
-                {
-                    !imageUrl ? <img src={FILM_CHICKS_BACKGROUND_IMG} alt={'dummy-images'} className='object-cover h-[195px] md:h-[270px] w-full' />
-                        : <img src={TMDB_IMAGE_URL + imageUrl} alt={'movie-images'} className='object-cover h-[195px] md:h-[270px] w-full' />
-                }
+                <img src={TMDB_IMAGE_URL + imageUrl} alt={'movie-images'} className='object-cover h-[195px] md:h-[270px] w-full' />
             </div>
         </div>
     )

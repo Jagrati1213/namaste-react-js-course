5c31995e725e6ac71a42da4f28512309f5bd49f7
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-02 17:21:53 +0530

    JG: add dummy image

--- snap-gpt/src/utils/Constant.js
+++ snap-gpt/src/utils/Constant.js
@@ -2,7 +2,8 @@ export const FILM_CHICKS_BACKGROUND_IMG = 'https://png.pngtree.com/background/20
 
 export const FILM_CHICKS_AVTAR_IMG = 'https://i.pinimg.com/originals/34/62/d2/3462d27440aa255b1c314ff16f4032b4.png';
 
-export const TMDB_IMAGE_URL = 'https://image.tmdb.org/t/p/w200'
+export const TMDB_IMAGE_URL = 'https://image.tmdb.org/t/p/w200';
+export const CAST_DUMMY_IMAGE = 'https://static.vecteezy.com/system/resources/previews/008/442/086/non_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg';
 export const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
 export const API_OPTIONS = {

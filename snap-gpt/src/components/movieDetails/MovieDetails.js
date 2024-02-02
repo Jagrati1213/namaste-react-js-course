3fbfe4cf58491560c031db9ffc2a92aa6a0b96c4
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-02 12:39:48 +0530

    JG: add condition on empty cast

--- snap-gpt/src/components/movieDetails/MovieDetails.js
+++ snap-gpt/src/components/movieDetails/MovieDetails.js
@@ -7,18 +7,19 @@ import { MovieList } from '../secondaryContainer/MovieList';
 export const MovieDetails = () => {
     const { movie, casts, similarMovies } = useSelector((store) => store.movies.movieDetails);
     if (!movie || !casts) return;
-    const popularCast = casts.filter((cast) => cast.popularity >= 40);
-
     return (
         <div className='text-white w-full my-4 px-4'>
             {/* mainContainer */}
             <MainContainer original_title={movie?.original_title} overview={movie?.overview} id={movie?.id} />
-
-            {/* Movie Casts */}
-            <div className='text-lg md:text-2xl font-bold px-2 mt-3'>
-                <h1>Casts</h1>
-            </div>
-            <MovieCasts casts={popularCast} />
+            {
+                casts.length > 0 && <>
+                    {/* Movie Casts */}
+                    <div className='text-lg md:text-2xl font-bold px-2 mt-3'>
+                        <h1>Casts</h1>
+                    </div>
+                    <MovieCasts casts={casts} />
+                </>
+            }
 
             {/* Similar movies */}
             <MovieList title={'Customers also watched'} movies={similarMovies} />

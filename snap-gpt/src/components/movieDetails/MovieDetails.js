dd061e9babfa69ac4691f621b09c97ba93fa3740
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-02 17:48:29 +0530

    JG: import custom hooks

--- snap-gpt/src/components/movieDetails/MovieDetails.js
+++ snap-gpt/src/components/movieDetails/MovieDetails.js
@@ -3,25 +3,38 @@ import { MainContainer } from '../mainContainer/MainContainer';
 import { useSelector } from 'react-redux';
 import { MovieCasts } from './MovieCasts';
 import { MovieList } from '../secondaryContainer/MovieList';
+import { CastList } from '../skeletons/movieDetails/CastList';
+import { useParams } from 'react-router-dom';
+import { useGetMovieDetails } from '../../utils/hooks/useGetMovieDetail';
+import { useGetMovieCasts } from '../../utils/hooks/useGetMovieCast';
+import { useGetSimilarMovies } from '../../utils/hooks/useGetSimilarMovies';
 
 export const MovieDetails = () => {
-    const { movie, casts, similarMovies } = useSelector((store) => store.movies.movieDetails);
-    if (!movie || !casts) return;
+
+    const { movie_id, movie_name } = useParams();
+    useGetMovieDetails(movie_name);
+    useGetMovieCasts(movie_id);
+    useGetSimilarMovies(movie_id);
+
+    const { movieDetails, casts, similarMovies } = useSelector((store) => store.movies);
+    console.log(movieDetails, casts, similarMovies);
+    if (!movieDetails) return;
+
     return (
         <div className='text-white w-full my-4 px-4'>
             {/* mainContainer */}
-            <MainContainer original_title={movie?.original_title} overview={movie?.overview} id={movie?.id} />
+            <MainContainer
+                original_title={movieDetails?.original_title}
+                overview={movieDetails?.overview}
+                id={movieDetails?.id} />
+
+            <h1>{casts?.length}</h1>
+            {/* movieCast */}
             {
-                casts.length > 0 && casts[0]?.profile_path && <>
-                    {/* Movie Casts */}
-                    <div className='text-lg md:text-2xl font-bold px-2 mt-3'>
-                        <h1>Casts</h1>
-                    </div>
-                    <MovieCasts casts={casts} />
-                </>
+                casts?.length > 0 && <MovieCasts casts={casts} />
             }
 
-            {/* Similar movies */}
+            {/* similarMovies */}
             <MovieList title={'Customers also watched'} movies={similarMovies} />
         </div>
     )

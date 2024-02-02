7ec13b5d45ad326a59413a9c442b44917a8ed9fa
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-02 18:30:11 +0530

    JG: add skeleton

--- snap-gpt/src/components/movieDetails/MovieCasts.js
+++ snap-gpt/src/components/movieDetails/MovieCasts.js
@@ -1,29 +1,38 @@
 import React from 'react'
-import { TMDB_IMAGE_URL } from '../../utils/Constant';
+import { CAST_DUMMY_IMAGE, TMDB_IMAGE_URL } from '../../utils/Constant';
 import { CastList } from '../skeletons/movieDetails/CastList';
 
 export const MovieCasts = ({ casts }) => {
 
-    if (!casts) return <CastList />
     return (
-        <div className={`flex overflow-x-scroll overflow-y-hidden gap-2`}>
-            {casts.map((actor, index) => (
-                actor.profile_path && <div
-                    key={index}
-                    className="cursor-pointer hover:bg-gray-800 p-2 rounded-sm shadow-md hover:shadow-lg transition duration-300 ease-in-out">
-                    <div className='w-32 md:w-36 h-[155px] md:h-[175px] overflow-hidden'>
-                        <img
-                            src={`${TMDB_IMAGE_URL}/${actor?.profile_path}`}
-                            alt={actor?.name}
-                            className='object-cover h-[150px] md:h-[170px] w-full ' />
-                    </div>
+        <>
+            <div className='text-lg md:text-2xl font-bold px-2 mt-3'>
+                <h1>Casts</h1>
+            </div>
+            {
+                !casts
+                    ? <CastList />
+                    : <div className={`flex overflow-x-scroll overflow-y-hidden gap-2`}>
+                        {casts.map((actor, index) => (
+                            <div
+                                key={index}
+                                className="cursor-pointer hover:bg-gray-800 p-2 rounded-sm shadow-md hover:shadow-lg transition duration-300 ease-in-out w-36 md:w-40">
+                                <div className='w-32 md:w-36 h-[155px] md:h-[175px] bg-gray-800 overflow-hidden'>
+                                    <img
+                                        src={actor?.profile_path ? `${TMDB_IMAGE_URL}/${actor?.profile_path}` : `${CAST_DUMMY_IMAGE}`}
+                                        alt={actor?.name}
+                                        loading='lazy'
+                                        className='object-cover h-[150px] md:h-[170px] w-full ' />
 
-                    <div className="flex mt-2 flex-col">
-                        <p className="text-green-400 font-semibold text-base md:text-lg">{actor?.name}</p>
-                        <p className="text-white text-xs md:text-base">{actor?.character}</p>
+                                </div>
+                                <div className="flex mt-2 flex-col text-wrap">
+                                    <p className="text-green-400 font-semibold text-base md:text-lg">{actor?.name}</p>
+                                    <p className="text-white text-xs md:text-base">{actor?.character}</p>
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                </div>
-            ))}
-        </div>
+            }
+        </>
     );
 }

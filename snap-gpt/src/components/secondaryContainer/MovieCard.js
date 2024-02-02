1ced852f1653fbd44b84bfe1c1c1cc20d1dd3865
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-02 12:19:59 +0530

    JG: add responsive into dummy image

--- snap-gpt/src/components/secondaryContainer/MovieCard.js
+++ snap-gpt/src/components/secondaryContainer/MovieCard.js
@@ -7,7 +7,7 @@ function MovieCard({ imageUrl }) {
         <div className='flex'>
             <div className='w-32 md:w-48 h-[200px] md:h-[275px] overflow-hidden'>
                 {
-                    !imageUrl ? <img src={FILM_CHICKS_BACKGROUND_IMG} alt={'dummy-images'} className='object-cover h-[270px] w-full' />
+                    !imageUrl ? <img src={FILM_CHICKS_BACKGROUND_IMG} alt={'dummy-images'} className='object-cover h-[195px] md:h-[270px] w-full' />
                         : <img src={TMDB_IMAGE_URL + imageUrl} alt={'movie-images'} className='object-cover h-[195px] md:h-[270px] w-full' />
                 }
             </div>

e3eafbba08feb6f5fa6c08e8a3869eff2dc4c9e4
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-02 12:20:36 +0530

    JG: call the getSimilarMovies and add into store

--- snap-gpt/src/components/secondaryContainer/MovieList.js
+++ snap-gpt/src/components/secondaryContainer/MovieList.js
@@ -5,6 +5,7 @@ import { getMovieDetail } from '../../utils/helper/getMovieDetail';
 import { useDispatch } from 'react-redux';
 import { addSingleMovieDetails } from '../../utils/redux/slice/MovieSlice';
 import { getMovieCast } from '../../utils/helper/getMovieCast';
+import { getSimilarMovies } from '../../utils/helper/getSimilarMovies';
 
 function MovieList({ title, movies }) {
 
@@ -15,14 +16,15 @@ function MovieList({ title, movies }) {
     const getData = async (arr) => {
         const movies = await getMovieDetail(arr[0]);
         const casts = await getMovieCast(arr[1]);
+        const similarMovies = await getSimilarMovies(arr[1]);
 
         // filter the related movie details and set into movies store 
         const singleMovieDetails = movies.find((item) => item?.original_title === arr[0]);
-        dispatch(addSingleMovieDetails({ movie: singleMovieDetails, casts: casts }));
+        dispatch(addSingleMovieDetails({ movie: singleMovieDetails, casts: casts, similarMovies: similarMovies }));
     }
 
     return (
-        <div className='px-4 md:px-10'>
+        <div className='px-4 md:px-2'>
             <div className='py-10 text-lg md:text-2xl font-bold'>
                 <h1>{title}</h1>
             </div>

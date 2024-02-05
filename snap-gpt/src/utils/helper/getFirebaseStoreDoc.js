74c6e693e8e2d08bbe2860e65068e0fa32a80b2c
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-05 18:52:57 +0530

    JG: add space

--- snap-gpt/src/utils/helper/getFirebaseStoreDoc.js
+++ snap-gpt/src/utils/helper/getFirebaseStoreDoc.js
@@ -2,8 +2,10 @@ import { doc, getDoc } from 'firebase/firestore';
 import { firestore } from '../firebase/Firebase';
 
 export const getFirebaseStoreDoc = async (uid) => {
+
     const docRef = doc(firestore, "users", uid);
     const docSnap = await getDoc(docRef);
+
     if (docSnap.exists()) {
         return docSnap.data();
     } else {

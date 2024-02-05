9516dfa445810d601de8941ae3e1ba97e56a69cb
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-05 15:47:13 +0530

    JG: return the doc

---
+++ snap-gpt/src/utils/helper/getFirebaseStoreDoc.js
@@ -0,0 +1,12 @@
+import { doc, getDoc } from 'firebase/firestore';
+import { firestore } from '../firebase/Firebase';
+
+export const getFirebaseStoreDoc = async (uid) => {
+    const docRef = doc(firestore, "users", uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+        return docSnap.data();
+    } else {
+        return null;
+    }
+}

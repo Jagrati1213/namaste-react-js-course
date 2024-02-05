08435fe239aae45f9dbeb8b5a7cc6ba73631f04c
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-05 14:23:55 +0530

    JG: clean browse page

--- snap-gpt/src/components/browse/Browse.js
+++ snap-gpt/src/components/browse/Browse.js
@@ -1,22 +1,16 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
 import { useGetNowPlayingMovies } from '../../utils/hooks/useGetNowPlayingMovies';
-import { MainContainer } from '../mainContainer/MainContainer';
-import { SecondaryContainer } from '../secondaryContainer/SecondaryContainer';
 import { useGetTopRatedMovie } from '../../utils/hooks/useGetTopRatedMovie';
 import { useGetUpComingMovies } from '../../utils/hooks/useGetUpComingMovies';
-import { Recommendation } from '../recommendation/Recommendation';
 import { Outlet } from 'react-router-dom';
 
 function Browse() {
 
-    // Get Gpt from the store state
-    const gpt = useSelector((store) => store.gpt);
-
-    // Called the method from movieSlice
+    // Called the method from movieSlice           
     useGetNowPlayingMovies();
     useGetTopRatedMovie();
     useGetUpComingMovies();
+
 
     return (
         <div className='text-white'>

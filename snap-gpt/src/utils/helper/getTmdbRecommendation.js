511f4fe7bd03c523464e1937672b1443d280fb24
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-06 17:40:01 +0530

    JG: optimize the code

--- snap-gpt/src/utils/helper/getTmdbRecommendation.js
+++ snap-gpt/src/utils/helper/getTmdbRecommendation.js
@@ -1,22 +1,36 @@
 import { Openai } from "../openai";
 import { getMovieDetail } from "./getMovieDetail";
+import OpenAI from 'openai';
 
 /**
- * A function Fetch the gives movie suggestion based on the search query.
- * @params searchValue : A function take search query as parameter for run the openai query.
- * @returns movieResults, movieNames : A function return movies result and movies name.
+ * A function use to fetch data with the help of GPT & gives suggested Movies based on the search query.
+ * @param searchValue - A value to get movies data.
+ * @param user - A user to check validity of their OpenAi Key.
+ * @return `movieResults` & `movieNames` : Returns fetched movies result and name.
  * 
  * */
-export const getTmdbRecommendation = async (searchValue) => {
+export const getTmdbRecommendation = async (searchValue, user = null) => {
 
-    // Set more information to get efficient result
+    // Set query
     const gptQuery = `Act as a movie recommendation system and suggest some movies for the query : ${searchValue}. Only give me 5 movies, comma separated like a example : Koi Mil Gaya, Dhamaal, Dhol, Bhagam Bhaag, Don.`
 
     //  Make a api call 
-    const gptQueryResult = await Openai.chat.completions.create({
-        messages: [{ role: 'user', content: gptQuery }],
-        model: 'gpt-3.5-turbo',
+    const userOpenai = new OpenAI({
+        apiKey: user?.openAiKey,
+        dangerouslyAllowBrowser: true
     });
+    let gptQueryResult = null;
+
+    // Check User openAi key
+    user?.openAiKey === null ?
+        gptQueryResult = await Openai.chat.completions.create({
+            messages: [{ role: 'user', content: gptQuery }],
+            model: 'gpt-3.5-turbo',
+        })
+        : gptQueryResult = await userOpenai.chat.completions.create({
+            messages: [{ role: 'user', content: gptQuery }],
+            model: 'gpt-3.5-turbo',
+        });
 
     // If no choices present
     if (!gptQueryResult?.choices[0]) return <h1>GPT, can't find anything...!!</h1>
@@ -24,10 +38,10 @@ export const getTmdbRecommendation = async (searchValue) => {
     // Create Array of related movies name
     const gptSuggestMovieNames = gptQueryResult?.choices[0]?.message?.content.split(',');
 
-    // //Return group of promises
+    // Return group of promises
     const moviesPromiseArray = gptSuggestMovieNames.map((movieName) => getMovieDetail(movieName));
 
-    // Convert these promise into array
+    // Convert promises into array
     const TmdbSuggestMovieResults = await Promise.all(moviesPromiseArray);
 
     return [TmdbSuggestMovieResults, gptSuggestMovieNames];

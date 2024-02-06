7cc526d2bac465e3876eee45527aef6d5fbc4e5d
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-06 19:27:31 +0530

    Jg: remove config

--- snap-gpt/src/utils/redux/store.js
+++ snap-gpt/src/utils/redux/store.js
@@ -2,13 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import UserSlice from './slice/UserSlice';
 import MovieSlice from './slice/MovieSlice';
 import GptSlice from './slice/GptSlice';
-import configSlice from './slice/Config';
 
 export const appStore = configureStore({
     reducer: {
         user: UserSlice,
         movies: MovieSlice,
         gpt: GptSlice,
-        config: configSlice,
     }
 })

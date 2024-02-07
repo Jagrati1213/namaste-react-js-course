e9fc0953c1c11ff3142973dd66adabf4e7d5a62f
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 13:02:27 +0530

    JG: change parent style

--- snap-gpt/src/components/skeletons/movieDetails/CastListSkeleton.js
+++ snap-gpt/src/components/skeletons/movieDetails/CastListSkeleton.js
@@ -3,7 +3,7 @@ import { CastCardSkeleton } from './CastCardSkeleton'
 
 export const CastListSkeleton = () => {
     return (
-        <div className='flex w-max gap-4 mt-10'>
+        <div className='flex w-max gap-4 px-2 md:px-6 bg-black bg-opacity-50 mt-3'>
             {
                 Array(4).fill(null).map((_, i) => {
                     return <CastCardSkeleton key={i} />

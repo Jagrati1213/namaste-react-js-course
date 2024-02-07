d202357767c2f980686a8a3ba3910bb2b60aec49
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 11:51:38 +0530

    Jg: change bg-color

--- snap-gpt/src/components/skeletons/movieDetails/CastCardSkeleton.js
+++ snap-gpt/src/components/skeletons/movieDetails/CastCardSkeleton.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export const CastCardSkeleton = () => {
     return (
-        <div className="h-40 md:h-52 w-40 rounded-sm bg-[#bdbbbb8f] animate-pulse"></div>
+        <div className="h-40 md:h-52 w-40 rounded-sm bg-gray-800 animate-pulse"></div>
     )
 }

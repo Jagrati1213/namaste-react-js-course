921049d451f933d1b02f74b086622014849c20ed
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 13:03:03 +0530

    JG: add loader

--- snap-gpt/src/components/movieDetails/MovieDetails.js
+++ snap-gpt/src/components/movieDetails/MovieDetails.js
@@ -7,16 +7,18 @@ import { useParams } from 'react-router-dom';
 import { useGetMovieDetails } from '../../utils/hooks/useGetMovieDetail';
 import { useGetMovieCasts } from '../../utils/hooks/useGetMovieCast';
 import { useGetSimilarMovies } from '../../utils/hooks/useGetSimilarMovies';
-import { CastList } from '../skeletons/movieDetails/CastList';
+import { CastListSkeleton } from '../skeletons/movieDetails/CastListSkeleton';
+import MovieListSekeleton from '../skeletons/movieList/MovieListSekeleton';
 
 export const MovieDetails = () => {
 
     const { movie_id, movie_name } = useParams();
+    const { movieDetails, casts, similarMovies, loading } = useSelector((store) => store.movies);
+
     useGetMovieDetails(movie_name);
     useGetMovieCasts(movie_id);
     useGetSimilarMovies(movie_id);
 
-    const { movieDetails, casts, similarMovies } = useSelector((store) => store.movies);
     if (!movieDetails) return;
 
     return (
@@ -30,11 +32,13 @@ export const MovieDetails = () => {
             {/* movieCast */}
             <h1 className='text-3xl text-white'>{!casts}</h1>
             {
-                casts.length === 0 ? <CastList /> : <MovieCasts casts={casts} />
+                loading.casts ? <CastListSkeleton /> : <MovieCasts casts={casts} />
             }
 
             {/* similarMovies */}
-            <MovieList title={'Customers also watched'} movies={similarMovies} />
+            {
+                loading.similarMovies ? <MovieListSekeleton /> : <MovieList title={'Customers also watched'} movies={similarMovies} />
+            }
         </div>
     )
 }

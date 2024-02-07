d9bd1d5f488e7879a3df82d1d484cd49efff607d
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 12:06:38 +0530

    JG: remove justify

--- snap-gpt/src/components/skeletons/movieList/MovieListSekeleton.js
+++ snap-gpt/src/components/skeletons/movieList/MovieListSekeleton.js
@@ -3,7 +3,7 @@ import MovieCardSkeleton from './MovieCardSkeleton'
 
 function MovieListSekeleton() {
     return (
-        <div className='flex flex-wrap justify-center gap-4 px-2 md:px-6 bg-black bg-opacity-50'>
+        <div className='flex flex-wrap gap-4 px-2 md:px-6 bg-black bg-opacity-50'>
             {Array(10).fill(null).map((_, i) => {
                 return <MovieCardSkeleton key={i} />
             })

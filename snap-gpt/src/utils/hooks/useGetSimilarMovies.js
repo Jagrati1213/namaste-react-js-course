c2b6a233e1cf67bf8c789199e1677463a4916c13
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 13:03:52 +0530

    Jg: add loader condition

--- snap-gpt/src/utils/hooks/useGetSimilarMovies.js
+++ snap-gpt/src/utils/hooks/useGetSimilarMovies.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { API_OPTIONS } from "../Constant";
 import { useDispatch } from "react-redux";
-import { addSimilarMovies } from "../redux/slice/MovieSlice";
+import { addSimilarMovies, setLoading } from "../redux/slice/MovieSlice";
 
 /**
 *  A function that fetch given movie id related similar movies from TMDB movie similar API.
@@ -15,9 +15,12 @@ export const useGetSimilarMovies = (movieId) => {
 
     // Function to fetch TMDB data && set into store
     const getSimilarMovies = async () => {
+
+        dispatch(setLoading({ dataKey: 'similarMovies', loadingState: true }))
         const response = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/similar?language=en-US&page=1`, API_OPTIONS);
         const data = await response.json();
         dispatch(addSimilarMovies(data?.results));
+        dispatch(setLoading({ dataKey: 'similarMovies', loadingState: false }))
     }
     // Called method after component mount
     useEffect(() => {

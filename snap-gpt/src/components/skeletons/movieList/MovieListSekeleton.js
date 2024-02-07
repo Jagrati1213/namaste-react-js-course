5cdfee55db4b638edeee29f45c79b48cd38a4113
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 13:01:44 +0530

    JG: optimize the code

--- snap-gpt/src/components/skeletons/movieList/MovieListSekeleton.js
+++ snap-gpt/src/components/skeletons/movieList/MovieListSekeleton.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import MovieCardSkeleton from './MovieCardSkeleton'
 
-function MovieListSekeleton() {
+function MovieListSekeleton({ number = 5 }) {
     return (
-        <div className='flex flex-wrap gap-4 px-2 md:px-6 bg-black bg-opacity-50'>
-            {Array(10).fill(null).map((_, i) => {
+        <div className='flex w-max gap-4 px-2 md:px-6 bg-black bg-opacity-50 mt-3'>
+            {Array(number).fill(null).map((_, i) => {
                 return <MovieCardSkeleton key={i} />
             })
             }
-
         </div>
     )
 }

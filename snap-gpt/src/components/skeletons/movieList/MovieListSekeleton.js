ed31b200a21b44b553a337c08e26830399f798b5
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 13:20:46 +0530

    Jg: add bg-color

--- snap-gpt/src/components/skeletons/movieList/MovieListSekeleton.js
+++ snap-gpt/src/components/skeletons/movieList/MovieListSekeleton.js
@@ -3,7 +3,7 @@ import MovieCardSkeleton from './MovieCardSkeleton'
 
 function MovieListSekeleton({ number = 5 }) {
     return (
-        <>
+        <div className=' bg-black bg-opacity-50'>
             <h1 className="m-5 w-96 h-[20px] px-2 md:px-6 rounded-sm bg-[#bdbbbb8f] animate-pulse"></h1>
             <div className='flex w-max gap-4 px-2 md:px-6 bg-black bg-opacity-50 mt-3'>
                 {Array(number).fill(null).map((_, i) => {
@@ -11,7 +11,7 @@ function MovieListSekeleton({ number = 5 }) {
                 })
                 }
             </div>
-        </>
+        </div>
     )
 }
 

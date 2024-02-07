5482d9bcc2414fb30703a69090edf40ef7658a36
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 12:51:04 +0530

    JG: add max width

--- snap-gpt/src/components/skeletons/movieDetails/CastListSkeleton.js
+++ snap-gpt/src/components/skeletons/movieDetails/CastListSkeleton.js
@@ -3,9 +3,9 @@ import { CastCardSkeleton } from './CastCardSkeleton'
 
 export const CastListSkeleton = () => {
     return (
-        <div className='flex flex-wrap gap-4 mt-10 w-full'>
+        <div className='flex w-max gap-4 mt-10'>
             {
-                Array(6).fill(null).map((_, i) => {
+                Array(4).fill(null).map((_, i) => {
                     return <CastCardSkeleton key={i} />
                 })
             }

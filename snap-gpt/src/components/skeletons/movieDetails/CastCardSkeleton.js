5dc52747dcc71b8a6bf6fc7177344e5eef501247
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 15:20:06 +0530

    Remove snap-gpt folder from original repository

--- snap-gpt/src/components/skeletons/movieDetails/CastCardSkeleton.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const CastCardSkeleton = () => {
-    return (
-        <div className="h-40 md:h-52 w-28 md:w-40 rounded-sm bg-[#bdbbbb8f] animate-pulse"></div>
-    )
-}
--- snap-gpt/src/utils/openai.js
+++
@@ -1,6 +0,0 @@
-import OpenAI from 'openai';
-
-export const Openai = new OpenAI({
-    apiKey: process.env.REACT_APP_OPENAI_KEY,
-    dangerouslyAllowBrowser: true
-});

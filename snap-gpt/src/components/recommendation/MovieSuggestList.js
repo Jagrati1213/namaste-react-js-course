cab129d97fb56b05527900cc9cf7e1e11f377969
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 13:20:34 +0530

    Jg:make more attractive

--- snap-gpt/src/components/recommendation/MovieSuggestList.js
+++ snap-gpt/src/components/recommendation/MovieSuggestList.js
@@ -7,7 +7,11 @@ export const MovieSuggestList = () => {
 
     // Get state from store
     const { gptMoviesNames, gptMoviesResult, loading } = useSelector((store) => store.gpt);
-    if (loading) return <MovieListSekeleton />;
+    if (loading) {
+        return Array(3).fill(null).map((_, i) => {
+            return <MovieListSekeleton key={i} />
+        })
+    };
 
     return (
         <div className='px-2 md:px-6 bg-black bg-opacity-50'>

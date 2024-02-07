86fe2b81c64e7a9de5fcec2ed0a12c955eae1e4c
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 11:45:13 +0530

    JG: add loader

--- snap-gpt/src/components/recommendation/MovieSuggestList.js
+++ snap-gpt/src/components/recommendation/MovieSuggestList.js
@@ -6,14 +6,13 @@ import MovieListSekeleton from '../skeletons/movieList/MovieListSekeleton';
 export const MovieSuggestList = () => {
 
     // Get state from store
-    const { gptMoviesNames, gptMoviesResult } = useSelector((store) => store.gpt);
-
-    if (gptMoviesResult?.length === 0) return <MovieListSekeleton />;
+    const { gptMoviesNames, gptMoviesResult, loading } = useSelector((store) => store.gpt);
+    if (loading) return <MovieListSekeleton />;
 
     return (
         <div className='px-2 md:px-6 bg-black bg-opacity-50'>
             {
-                gptMoviesNames?.map((item, index) => < MovieList key={index} title={item} movies={gptMoviesResult[index]} />)
+                gptMoviesNames?.map((item, index) => <MovieList key={index} title={item} movies={gptMoviesResult[index]} />)
             }
         </div>
     )

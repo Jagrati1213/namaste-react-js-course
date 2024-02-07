2ac407f26dacaa324d0c3159e7ba16c7120584a0
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-07 12:52:29 +0530

    JG: add optional chaining

--- snap-gpt/src/components/secondaryContainer/MovieList.js
+++ snap-gpt/src/components/secondaryContainer/MovieList.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import { MovieCard } from './MovieCard';
 import { Link } from 'react-router-dom';
-import MovieListSekeleton from '../skeletons/movieList/MovieListSekeleton';
 
 function MovieList({ title, movies }) {
-
-    if (!movies) return <MovieListSekeleton />;
 
     return (
         <div className='px-4 md:px-2'>
             <div className='py-10 text-lg md:text-2xl font-bold'>
                 <h1>{title}</h1>
             </div>
-            <div className={`flex ${movies.length > 4 ? 'overflow-x-scroll' : ''} overflow-y-hidden gap-4`}>
+            <div className={`flex ${movies?.length > 4 ? 'overflow-x-scroll' : ''} overflow-y-hidden gap-4`}>
                 {
                     movies?.map((movie) => {
                         return <Link key={movie?.id} to={`/browse/${movie?.id}/${movie?.original_title}`}>

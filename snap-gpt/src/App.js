9048105f5f4b0f29485368ad7dce1d1542606286
Jagrati Gupta <123jagratigupta@gmail.com>
2024-02-08 19:32:47 +0530

    JG: optimize UI

--- snap-gpt/src/App.js
+++ snap-gpt/src/App.js
@@ -3,16 +3,18 @@ import Body from './components/body/Body';
 import Header from './components/header/Header';
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from './utils/firebase/Firebase';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { addUser, removeUser } from './utils/redux/slice/UserSlice';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { getFirebaseStoreDoc } from './utils/helper/getFirebaseStoreDoc';
+import { FILM_CHICKS_BACKGROUND_IMG } from './utils/Constant';
 
 function App() {
 
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
+  const gpt = useSelector((store) => store.gpt);
 
   useEffect(() => {
 
@@ -53,10 +55,14 @@ function App() {
   }, [dispatch, navigate]);
 
   return (
-    <div className='app max-w-screen-2xl mx-auto relative w-full min-h-[100vh] overflow-x-hidden'>
-      <Header />
-      <Body />
-    </div>
+    <section className='bg-no-repeat bg-fixed bg-cover bg-blend-screen'
+      style={gpt.showRecommendation || location.pathname === '/' ? { backgroundImage: `url(${FILM_CHICKS_BACKGROUND_IMG}) ` } : { background: "black" }}>
+
+      <div className='app max-w-screen-2xl mx-auto relative w-full min-h-[100vh] overflow-x-hidden'>
+        <Header />
+        <Body />
+      </div>
+    </section>
   );
 }
 
